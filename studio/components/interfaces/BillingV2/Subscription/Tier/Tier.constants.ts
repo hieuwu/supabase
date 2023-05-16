ed2315e3d3050bd7a15d989fefb5f8730333d840
Joshen Lim <joshenlimek@gmail.com>
2023-05-16 15:15:17 +0800

    Init tier selection side panel

---
+++ studio/components/interfaces/BillingV2/Subscription/Tier/Tier.constants.ts
@@ -0,0 +1,88 @@
+// Brought over from Plans.Constants.ts but ideally put in common folder (packages/data)
+
+import { STRIPE_PRODUCT_IDS } from 'lib/constants'
+
+export interface PricingInformation {
+  id: string
+  new: boolean
+  name: string
+  price?: number
+  preface?: string
+  features: string[]
+  footer?: string
+}
+
+export const PRICING_META: { [x: string]: PricingInformation } = {
+  [STRIPE_PRODUCT_IDS.FREE]: {
+    id: 'tier_free',
+    new: false,
+    name: 'Free',
+    price: 0,
+    preface: undefined,
+    features: [
+      'Unlimited API requests',
+      '500MB database size',
+      '1GB file storage size',
+      '2GB bandwidth',
+      '50MB storage uploads',
+      'Social auth Providers',
+      '50K MAUs',
+      '500K edge function invocations',
+      '1 day log retention',
+      'Community support',
+    ],
+    footer: undefined,
+  },
+  [STRIPE_PRODUCT_IDS.PRO]: {
+    id: 'tier_pro',
+    new: false,
+    name: 'Pro',
+    price: 25,
+    preface: undefined,
+    features: [
+      'Unlimited API requests',
+      '8GB database size included',
+      '100GB file storage size included',
+      '50GB bandwidth included',
+      '5GB storage uploads included',
+      'Social auth Providers',
+      '100K MAUs included',
+      '2M edge function invocations',
+      'Daily backups',
+      '7 day log retention',
+      'No project pausing',
+      'Email support',
+    ],
+    footer:
+      'Your cost control settings determine if you wish to pay for anything more than the above.',
+  },
+  [STRIPE_PRODUCT_IDS.TEAM]: {
+    id: 'tier_team',
+    new: true,
+    name: 'Team',
+    price: 599,
+    preface: 'Everything in Pro, plus:',
+    features: [
+      'Read only member role',
+      'Billing only member role',
+      'Standardised security questionnaire',
+      'SOC2',
+      'HIPAA (Coming soon)',
+      'SSO for Supabase Dashboard',
+      'Priority email support & SLAs',
+      '14 day daily backups',
+      '28 day log retention',
+    ],
+    footer:
+      'Your cost control settings determine if you wish to pay for anything more than the above.',
+  },
+  Enterprise: {
+    id: 'tier_enterprise',
+    new: false,
+    name: 'Enterprise',
+    price: undefined,
+    preface: 'For large-scale applications managing serious workloads',
+    features: [`Designated Support manager & SLAs`, `SSO/ SAML`, `Custom contracts & invoicing`],
+    footer: undefined,
+  },
+}
--- studio/components/interfaces/BillingV2/Subscription/Tier/TierUpdateSidePanel.tsx
+++ studio/components/interfaces/BillingV2/Subscription/Tier/TierUpdateSidePanel.tsx
@@ -1,4 +1,11 @@
-import { SidePanel } from 'ui'
+import clsx from 'clsx'
+import { useParams } from 'common'
+import { useProjectSubscriptionV2Query } from 'data/subscriptions/project-subscription-v2-query'
+import { useFlag } from 'hooks'
+import { PRICING_TIER_PRODUCT_IDS } from 'lib/constants'
+import { Button, IconCheck, SidePanel } from 'ui'
+import EnterpriseCard from './EnterpriseCard'
+import { PRICING_META } from './Tier.constants'
 
 export interface TierUpdateSidePanelProps {
   visible: boolean
@@ -6,9 +13,113 @@ export interface TierUpdateSidePanelProps {
 }
 
 const TierUpdateSidePanel = ({ visible, onClose }: TierUpdateSidePanelProps) => {
+  const { ref: projectRef } = useParams()
+  const { data: subscription, isLoading } = useProjectSubscriptionV2Query({ projectRef })
+
+  const userIsOnTeamTier = subscription?.tier?.supabase_prod_id === PRICING_TIER_PRODUCT_IDS.TEAM
+  // const teamTierEnabled = false
+  const teamTierEnabled = useFlag('teamTier') || userIsOnTeamTier
+
   return (
-    <SidePanel visible={visible} onCancel={onClose}>
-      Tier Update
+    <SidePanel
+      size="xxlarge"
+      visible={visible}
+      onCancel={onClose}
+      header="Change subscription plan"
+    >
+      <SidePanel.Content>
+        <div className="py-6 grid grid-cols-12 gap-3">
+          {Object.values(PRICING_META).map((plan) => {
+            const isCurrentPlan = subscription?.tier.supabase_prod_id === plan.id
+
+            if (plan.id === 'tier_team' && !teamTierEnabled) return null
+            if (plan.id === 'tier_enterprise') {
+              return (
+                <EnterpriseCard
+                  key={plan.id}
+                  plan={plan}
+                  isCurrentPlan={isCurrentPlan}
+                  isTeamTierEnabled={teamTierEnabled}
+                />
+              )
+            }
+
+            return (
+              <div
+                key={plan.id}
+                className={clsx(
+                  'border rounded-md px-4 py-4 flex flex-col items-start justify-between',
+                  teamTierEnabled && plan.id === 'tier_enterprise' ? 'col-span-12' : 'col-span-4',
+                  plan.id === 'tier_enterprise' ? 'bg-scale-200' : 'bg-scale-300'
+                )}
+              >
+                <div className="w-full">
+                  <div className="flex items-center space-x-2">
+                    <p className={clsx('text-brand-900 text-sm uppercase')}>{plan.name}</p>
+                    {isCurrentPlan ? (
+                      <div className="text-xs bg-scale-500 text-scale-1000 rounded px-2 py-0.5">
+                        Current plan
+                      </div>
+                    ) : plan.new ? (
+                      <div className="text-xs bg-brand-400 text-brand-900 rounded px-2 py-0.5">
+                        New
+                      </div>
+                    ) : (
+                      <></>
+                    )}
+                  </div>
+                  <div className="mt-4 flex items-center space-x-1">
+                    {(plan.price ?? 0) > 0 && <p className="text-scale-1000 text-sm">From</p>}
+                    <p className="text-scale-1200 text-lg">${plan.price}</p>
+                    <p className="text-scale-1000 text-sm">per month</p>
+                  </div>
+                  <div className={clsx('flex mt-1 mb-4', (plan.price ?? 0) === 0 && 'opacity-0')}>
+                    <div className="text-xs bg-brand-400 text-brand-900 rounded px-2 py-0.5">
+                      Usage based plan
+                    </div>
+                  </div>
+                  {isCurrentPlan ? (
+                    <Button block disabled type="default">
+                      Current plan
+                    </Button>
+                  ) : (
+                    <Button block type="primary">
+                      {subscription?.tier.supabase_prod_id !== PRICING_TIER_PRODUCT_IDS.FREE &&
+                      plan.id === PRICING_TIER_PRODUCT_IDS.FREE
+                        ? 'Downgrade'
+                        : 'Upgrade'}{' '}
+                      to {plan.name}
+                    </Button>
+                  )}
+
+                  <div className="border-t my-6" />
+
+                  <ul role="list">
+                    {plan.features.map((feature) => (
+                      <li key={feature} className="flex py-2">
+                        <div className="w-[12px]">
+                          <IconCheck
+                            className="h-3 w-3 text-brand-900 translate-y-[2.5px]"
+                            aria-hidden="true"
+                            strokeWidth={3}
+                          />
+                        </div>
+                        <p className="ml-3 text-xs text-scale-1100">{feature}</p>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+
+                {plan.footer && (
+                  <div className="border-t pt-4 mt-4">
+                    <p className="text-scale-1000 text-xs">{plan.footer}</p>
+                  </div>
+                )}
+              </div>
+            )
+          })}
+        </div>
+      </SidePanel.Content>
     </SidePanel>
   )
 }
